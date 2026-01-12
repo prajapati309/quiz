15fa218150be271a6cbf38988ce9aaa385aba46d
Akash Prajapati <prajapati3098@gmail.com>
2026-01-12 10:43:17 +0530

    Public quiz flow and scoring

---
+++ app/controllers/quizzes_controller.rb
@@ -0,0 +1,27 @@
+# app/controllers/quizzes_controller.rb
+class QuizzesController < ApplicationController
+  def show
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def submit
+    quiz = Quiz.find(params[:id])
+    answers = params[:answers] || {}
+
+    score = 0
+
+    quiz.questions.each do |q|
+      user_answer = answers[q.id.to_s]
+      correct = q.correct_answer.to_s.strip.downcase
+      score += 1 if user_answer.to_s.strip.downcase == correct
+    end
+
+    submission = quiz.submissions.create!(
+      answers: answers,
+      score: score,
+      total: quiz.questions.count
+    )
+
+    redirect_to quiz_path(quiz, submission_id: submission.id)
+  end
+end
